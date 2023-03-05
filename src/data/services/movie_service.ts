24b9405eb30ba1d98b975e7c178658b3f442c1e3
adisve <adis.veletanlic@gmail.com>
2023-03-05 02:06:26 +0100

    Add docstrings movie_service.ts

--- src/data/services/movie_service.ts
+++ src/data/services/movie_service.ts
@@ -4,6 +4,10 @@ import { Movie } from '../../domain/models/Movie'
 import { mapToMovies } from '../utils/mapping_utils'
 import { checkStatus } from '../axios/middleware_functons';
 
+/**
+ * Fetches movies from the server in sorted order, based on title.
+ * @returns Promise
+ */
 export const fetchMovies = async (): Promise<any> => {
   try {
         const response = await get(createRequestURL(moviesURL, { sort: "title" }));
@@ -14,6 +18,10 @@ export const fetchMovies = async (): Promise<any> => {
     }
 };
 
+/**
+ * Fetches screenings from the server in no particular order
+ * @returns Promise
+ */
 export const fetchScreenings = async (): Promise<any> => {
   try {
         const response = await get(screeningsURL);
@@ -24,20 +32,37 @@ export const fetchScreenings = async (): Promise<any> => {
     }
 };
 
+/**
+ * Fetches movies from the server specifically for the hero section,
+ * as it only retrieves 4 movies at a time.
+ * @returns Promise
+ */
 export const fetchHeroMovies = async (): Promise<any> => {
     try {
-        const response_1 = await get(createRequestURL(moviesURL, { 'limit': 4, 'sort': '-title' }), [checkStatus]);
-        return response_1.data;
+        const response = await get(createRequestURL(moviesURL, { 'limit': 4, 'sort': '-title' }), [checkStatus]);
+        return response.data;
     } catch (error) {
         console.error(error);
         throw new Error('Request timed out');
     }
   };
 
+/**
+ * Sorts movies by their most recent screening date
+ * @param movies 
+ * @returns List of movies
+ */
 export const sortMoviesByScreeningDate = (movies: Movie[]): Movie[] => {
   return movies.sort((a, b) => a.screenings[0].getTime() - b.screenings[0].getTime());
 };
 
+/**
+ * Maps the movies to a list of movies, along with each movie's available
+ * screening dates as a list of dates, if the screenings are passed as well.
+ * @param moviesData 
+ * @param screeningsData 
+ * @returns List of movies mapped to screenings if it is passed as parameter
+ */
 export const mapMoviesDataToModel = (moviesData: any[], screeningsData?: any[]): Movie[] => {
   const movies: Movie[] = mapToMovies(moviesData);
   const screeningsDict: { [id: number]: Date[] } = {};
